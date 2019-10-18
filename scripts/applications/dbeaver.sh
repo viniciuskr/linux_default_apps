2511ce1b6ab790600383a8f2def876f88a9f433f
Vinicius Campos da Rosa Krauspenhar (GOVBR - DDP) <vinicius.krauspenhar@govbr.com.br>
2019-10-18 08:58:09 -0300

    Initial scripts

---
+++ scripts/applications/dbeaver.sh
@@ -0,0 +1,2 @@
+wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb 
+sudo apt install -y ./dbeaver-ce_latest_amd64.deb
---
+++ scripts/main.sh
@@ -0,0 +1,4 @@
+for filename in ./applications/*.sh; do     
+  source filename 
+done
+
